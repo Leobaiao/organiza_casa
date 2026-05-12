5bed07b778ce61ffa7a50bae4d10ffb92fe4d493
Leobaiao <leobaiao966@gmail.com>
2026-05-12 15:44:56 -0300

    chore: remove temporary sql migration files

--- add_pix_and_receipts.sql
+++
@@ -1,7 +0,0 @@
--- Add pix_key to households
-ALTER TABLE households ADD COLUMN IF NOT EXISTS pix_key TEXT;
-
--- Add receipt_url to transactions to store the proof of payment
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS receipt_url TEXT;
-
--- Create a storage bucket for receipts if not exists (via SQL is harder, but we'll assume it's created via UI or we'll use a public URL)
